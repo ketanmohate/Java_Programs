5c76b4e136a437c0354ca2cdbc53689868a30ff8
ketanmohate <ketanmohate9595@gmail.com>
2025-03-02 18:16:08 +0530

    Q1

--- Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
+++ Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
@@ -2,6 +2,13 @@ package org.test;
 
 public class Q1_Pattren1 {
 	public static void main(String[] args) {
-
+		for(int i=1 ; i<=5 ; i++)
+		{
+			for(int j=1; j<=5 ; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 	}
 }
