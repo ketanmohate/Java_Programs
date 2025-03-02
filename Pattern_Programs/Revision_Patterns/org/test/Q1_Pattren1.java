d615f67a871d56e4f295b3d7100d6f6417fa1aba
ketanmohate <ketanmohate9595@gmail.com>
2025-03-02 19:08:17 +0530

    update

--- Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
+++ Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
@@ -4,9 +4,17 @@ public class Q1_Pattren1 {
 	public static void main(String[] args) {
 		for(int i=1 ; i<=5 ; i++)
 		{
+			int count = 6-i;
 			for(int j=1; j<=5 ; j++)
 			{
-				System.out.print("* ");
+				if(i>=j)
+				{
+					System.out.print((count++) +" ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
 			}
 			System.out.println();
 		}
