fd825d219d94f0d5c96ec3e8a80864b2ebcbbdaf
ketanmohate <ketanmohate9595@gmail.com>
2025-03-02 19:10:32 +0530

    update

--- Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
+++ Pattern_Programs/Revision_Patterns/org/test/Q1_Pattren1.java
@@ -1,5 +1,13 @@
 package org.test;
 
+/*
+5         
+4 5       
+3 4 5     
+2 3 4 5   
+1 2 3 4 5
+ */
+
 public class Q1_Pattren1 {
 	public static void main(String[] args) {
 		for(int i=1 ; i<=5 ; i++)
